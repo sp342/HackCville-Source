028a616b894a8c77d37458c1ad9edbe660679009
Stephen P <stephen.palathingal@gmail.com>
2018-04-04 18:55:30 -0400

    regular update

---
+++ Project/src/Pictures.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+
+//PROPS: pictures (array of objects with url and title fields)
+
+export default class Pictures extends Component {
+    render() {
+        let picArray = this.props.pictures.map(picObj => {
+            return (
+                <div>
+                    <h1> {picObj.title} </h1>
+                    <img src={picObj.url} />
+                </div>
+            )
+        });
+        return (
+            <div>
+                {picArray}
+            </div>
+        )
+    }
+} 
